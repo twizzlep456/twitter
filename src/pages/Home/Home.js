4f33966c578f195360292d9c17726dce1ba7068b
twilight <kmatsix15@gmail.com>
2022-11-09 10:19:24 +0100

    new project twitter

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home(){
+    return(
+        <div className="home">
+            <div className="leftContainer">Left</div>
+            <div className="centerContainer">Center</div>
+            <div className="rightContainer">Right</div>
+        </div>
+    );
+}
+
+export default Home;
