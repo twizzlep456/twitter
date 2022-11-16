cd2e69715c0c24fee3f2ac70f06cda5cbbf2b7c6
twilight <kmatsix15@gmail.com>
2022-11-16 02:14:23 +0100

    twitter practice 2

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,20 @@
 import './Home.css';
+import Logo from '../../components/Home/Logo/Logo';
+import Feed from '../../components/Home/Feed/Feed';
+import LoginOption from '../../components/Home/LoginOption/LoginOption';
 
 function Home(){
     return(
         <div className="home">
-            <div className="leftContainer">Left</div>
-            <div className="centerContainer">Center</div>
-            <div className="rightContainer">Right</div>
+            <div className="leftContainer">
+                <Logo />     
+            </div>
+            <div className="centerContainer">
+                 <Feed/>
+            </div>
+            <div className="rightContainer">
+               <LoginOption/>
+            </div>
         </div>
     );
 }
